0b1104f31f23049d1c96b1e391761cc64f43b905
Abhiram Shibu <abhiramshibu1998@gmail.com>
2017-07-29 11:29:47 +0530

    Change lineage bootanimation to DOS animation
    Change-Id: Id5f0a4f56bce4301d34ab69e12e7b700524bd060

--- build/core/config.mk
+++ build/core/config.mk
@@ -16,8 +16,8 @@
 CM_SRC_API_DIR := $(TOPDIR)prebuilts/cmsdk/api
 INTERNAL_CM_PLATFORM_API_FILE := $(TARGET_OUT_COMMON_INTERMEDIATES)/PACKAGING/cm_public_api.txt
 INTERNAL_CM_PLATFORM_REMOVED_API_FILE := $(TARGET_OUT_COMMON_INTERMEDIATES)/PACKAGING/cm_removed.txt
-FRAMEWORK_CM_PLATFORM_API_FILE := $(TOPDIR)vendor/cmsdk/api/cm_current.txt
-FRAMEWORK_CM_PLATFORM_REMOVED_API_FILE := $(TOPDIR)vendor/cmsdk/api/cm_removed.txt
+FRAMEWORK_CM_PLATFORM_API_FILE := $(TOPDIR)vendor/dossdk/api/cm_current.txt
+FRAMEWORK_CM_PLATFORM_REMOVED_API_FILE := $(TOPDIR)vendor/dossdk/api/cm_removed.txt
 FRAMEWORK_CM_API_NEEDS_UPDATE_TEXT := $(TOPDIR)vendor/dos/build/core/apicheck_msg_current.txt
 
 BUILD_MAVEN_PREBUILT := $(TOP)/vendor/dos/build/core/maven_artifact.mk
